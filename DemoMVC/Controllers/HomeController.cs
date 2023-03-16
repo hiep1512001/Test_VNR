aa634788ba1d955a4b7023ab2d244c2fb0e2946a
hiep1512001 <67302956+hiep1512001@users.noreply.github.com>
2023-03-16 14:26:03 +0700

    update source code

---
+++ DemoMVC/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using DemoMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            VNR_InternShipEntities db = new VNR_InternShipEntities();
+            List<KhoaHoc> listKhoahoc = db.KhoaHocs.ToList();
+            MonHoc_KhoaHoc monHoc_KhoaHoc = new MonHoc_KhoaHoc();
+            monHoc_KhoaHoc.listKhoaHoc = listKhoahoc;
+            monHoc_KhoaHoc.listMonhoc = null;
+            monHoc_KhoaHoc.tenKhoaHoc = null;
+            return View(monHoc_KhoaHoc);
+        }
+
+        public ActionResult LoadTenMonHoc(int id)
+        {
+            VNR_InternShipEntities db = new VNR_InternShipEntities();
+            List<KhoaHoc> listKhoahoc = db.KhoaHocs.ToList();
+            List<MonHoc> listMonHoc = db.MonHocs.ToList();
+            List<MonHoc> a = new List<MonHoc>();
+            foreach(MonHoc monhoc in listMonHoc)
+            {
+                if (monhoc.KhoaHocID == id)
+                {
+                    a.Add(monhoc);
+                }
+            }
+            MonHoc_KhoaHoc monHoc_KhoaHoc = new MonHoc_KhoaHoc();
+            monHoc_KhoaHoc.tenKhoaHoc = db.KhoaHocs.Find(id).TenKhoaHoc;
+            monHoc_KhoaHoc.listKhoaHoc = listKhoahoc;
+            monHoc_KhoaHoc.listMonhoc = a;
+            return View(monHoc_KhoaHoc);
+        }
+    }
+}
---
+++ DemoMVC/Models/MonHoc_KhoaHoc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoMVC.Models
+{
+    public class MonHoc_KhoaHoc
+    {
+        public String tenKhoaHoc { get; set; }
+        public List<MonHoc> listMonhoc { get; set; }
+        public List<KhoaHoc> listKhoaHoc { get; set; }
+    }
+}
